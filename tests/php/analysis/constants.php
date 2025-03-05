8471f6e64b41800101c794fa86230088c9d0c776
Michael Bragg <michael@michaelbragg.com>
2025-03-05 12:19:52 +0000

    Added: PHPStan test config

---
+++ tests/php/analysis/constants.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * PHP Stan: Constants
+ *
+ * @package Vatu\WordPress\Tests
+ *
+ * phpcs:disable Squiz.PHP.DiscouragedFunctions,NeutronStandard.Constants.DisallowDefine
+ */
+
+declare(strict_types=1);
+
+define( 'WP_ENV', 'development' );
+define( 'WP_ENVIRONMENT_TYPE', 'development' );
+
+define( 'WPINC', 'wp-includes' );
