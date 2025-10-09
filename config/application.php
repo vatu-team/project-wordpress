f4ef28ad2caac9dae99d6cce653eceeaf9bd491f
Michael Bragg <michael@michaelbragg.com>
2025-10-09 11:06:48 +0000

    Fixed: Database url potentially being passed non string

--- config/application.php
+++ config/application.php
@@ -80,9 +80,14 @@ Config::define( key: 'DB_COLLATE', value: '' );
 // phpcs:ignore SlevomatCodingStandard.Variables.UnusedVariable.UnusedVariable
 $table_prefix = getenv( name: 'DB_PREFIX' ) ?: 'wp_';
 
-if ( getenv( name: 'DATABASE_URL' ) ) {
+/**
+ * @var string $database_url
+ */
+$database_url = is_string( getenv( name: 'DATABASE_URL' ) ) ? getenv( name: 'DATABASE_URL' ) : '';
+
+if ( $database_url !== '' ) {
 	// phpcs:ignore WordPress.WP.AlternativeFunctions.parse_url_parse_url
-	$dsn = (object) parse_url( url: strval( value: getenv( name: 'DATABASE_URL' ) ) );
+	$dsn = (object) parse_url( url: $database_url );
 
 	Config::define( key: 'DB_NAME', value: substr( string: $dsn->path, offset: 1 ) );
 	Config::define( key: 'DB_USER', value: $dsn->user );
